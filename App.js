c2c6867ab6dc646f94ac584d4acd4dac5de24a4f
Toygun Yildiz <toyguny@hotmail.com>
2023-10-14 19:46:00 +0300

    folders

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/Onboarding.js
@@ -0,0 +1,21 @@
+import React, { useState, useRef, useContext, useCallback } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    TextInput,
+    Pressable,
+    KeyboardAvoidingView,
+    Platform,
+} from "react-native";
+
+const Onboarding = () => {
+
+};
+
+const styles = StyleSheet ({
+
+});
+
+export default Onboarding;
